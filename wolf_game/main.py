d5d511bc17497be95778196dc65541a0d4c6a97d
msa <msai.sadeghi@gmail.com>
2023-11-04 20:55:42 +0330

    s

--- wolf_game/main.py
+++ wolf_game/main.py
@@ -1,5 +1,6 @@
 from random import randint
 import pygame
+from pyscreeze import center
 pygame.init()
 
 
@@ -16,7 +17,7 @@ pygame.display.set_icon(wolf)
 
 pygame.mixer.music.load("wolf_game/bgmusic.mp3")
 pygame.mixer.music.set_volume(0.3)
-pygame.mixer.music.play(-1)
+music_played = False
 
 catch_sound = pygame.mixer.Sound("wolf_game/Chomp.wav")
 
@@ -46,24 +47,33 @@ lives_rect = lives_text.get_rect()
 lives_rect.topright = (SCREEN_WIDTH, 0)
 
 
+welcome_text = font.render('Welcome To my Game', True, (255, 120, 10))
+welcome_rect = welcome_text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+
+start_time = pygame.time.get_ticks()
+
+
 game_over_text = font.render(
-    'Game Over, press Enter to play again...', True, (255, 0, 0))
+    'Game Over\npress Enter to play again...', True, (255, 0, 0))
 game_over_rect = game_over_text.get_rect()
 game_over_rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
 
 def pause_game():
-    global running
+    global running, score, lives
     screen.fill((0, 0, 0))
     screen.blit(game_over_text, game_over_rect)
     pygame.display.update()
-
+    score = 0
+    lives = 3
+    pygame.mixer.music.stop()
     is_paused = True
     while is_paused:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     is_paused = False
+                    pygame.mixer.music.play(-1)
 
             if event.type == pygame.QUIT:
                 is_paused = False
@@ -75,36 +85,49 @@ clock = pygame.time.Clock()
 
 running = True
 while running:
+    # event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and wolf_rect.top > 0:
-        wolf_rect.y -= 5
-    if keys[pygame.K_DOWN] and wolf_rect.bottom < SCREEN_HEIGHT:
-        wolf_rect.y += 5
-
-    sheep_rect.x += 5
-    if sheep_rect.x > SCREEN_WIDTH:
-        sheep_rect.topleft = (0, randint(100, SCREEN_HEIGHT - 48))
-        lives -= 1
-        if lives <= 0:
-            pause_game()
-
-    if wolf_rect.colliderect(sheep_rect):
-        score += 1
-        catch_sound.play()
-        sheep_rect.topleft = (0, randint(100, SCREEN_HEIGHT - 48))
-
-    score_text = font.render(f'Score: {score}', True, (255, 0, 0))
-    lives_text = font.render(f'lives: {lives}', True, (255, 0, 0))
+    # clear the main screen
     screen.fill((0, 0, 0))
-    screen.blit(wolf, wolf_rect)
-    screen.blit(sheep, sheep_rect)
-    screen.blit(title, title_rect)
-    screen.blit(score_text, score_rect)
-    screen.blit(lives_text, lives_rect)
+    # catch the next time
+    next_time = pygame.time.get_ticks()
+
+    if next_time - start_time < 3000:
+        screen.blit(welcome_text, welcome_rect)
+    else:
+        if not music_played:
+            print("blalallalalalla")
+            pygame.mixer.music.play(-1)
+            music_played = True
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and wolf_rect.top > 0:
+            wolf_rect.y -= 5
+        if keys[pygame.K_DOWN] and wolf_rect.bottom < SCREEN_HEIGHT:
+            wolf_rect.y += 5
+
+        sheep_rect.x += 5
+        if sheep_rect.x > SCREEN_WIDTH:
+            sheep_rect.topleft = (0, randint(100, SCREEN_HEIGHT - 48))
+            lives -= 1
+            if lives <= 0:
+                pause_game()
+
+        if wolf_rect.colliderect(sheep_rect):
+            score += 1
+            catch_sound.play()
+            sheep_rect.topleft = (0, randint(100, SCREEN_HEIGHT - 48))
+
+        score_text = font.render(f'Score: {score}', True, (255, 0, 0))
+        lives_text = font.render(f'lives: {lives}', True, (255, 0, 0))
+        screen.blit(wolf, wolf_rect)
+        screen.blit(sheep, sheep_rect)
+        screen.blit(title, title_rect)
+        screen.blit(score_text, score_rect)
+        screen.blit(lives_text, lives_rect)
 
     pygame.display.update()
     clock.tick(FPS)
